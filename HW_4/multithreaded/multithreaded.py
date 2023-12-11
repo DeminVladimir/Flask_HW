cb21a3e0106c684a9307034d0f48427fb266b7d2
Vladimir <dv.quiz@yandex.ru>
2023-12-11 22:58:03 +0300

    Home Work 4

---
+++ HW_4/multithreaded/multithreaded.py
@@ -0,0 +1,48 @@
+# Задание
+#
+# Написать программу, которая скачивает изображения с заданных URL-адресов и сохраняет их на диск.
+# Каждое изображение должно сохраняться в отдельном файле, название которого соответствует
+# названию изображения в URL-адресе.
+# Например, URL-адрес: https://example/images/image1.jpg -> файл на диске: image1.jpg
+# — Программа должна использовать многопоточный, многопроцессорный и асинхронный подходы.
+# — Программа должна иметь возможность задавать список URL-адресов через аргументы командной строки.
+# — Программа должна выводить в консоль информацию о времени скачивания каждого изображения и общем
+# времени выполнения программы.
+
+# Многопоточный
+
+
+import threading
+import time
+import requests
+from sys import argv
+
+start_time = time.time()
+all_time = 0
+urls = argv[1::]
+
+
+def thread_dow(url):
+    global all_time
+    res = requests.get(url)
+    file_name = url.split('/')
+    with open(file_name[-1], 'wb') as f:
+        f.write(res.content)
+    print(f'{time.time() - start_time:.2f}')
+    all_time += time.time() - start_time
+
+
+threads = []
+
+for url in urls:
+    thread = threading.Thread(target=thread_dow, args=[url])
+    threads.append(thread)
+    thread.start()
+    if url == urls[-1]:
+        check = True
+
+for thread in threads:
+    thread.join()
+
+if check:
+    print(f'{all_time:.2f}')
